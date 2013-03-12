879a9e636f2bce2606ea55329da79e9e39769a35
Daniel Jallov <danieljallov@gmail.com>
2013-03-12 18:23:18 +0100

    Added GUI with Restart button to level_1a

---
+++ Assets/GUIScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 80, 30), "Restart"))
+        {
+
+            Application.LoadLevel(Application.loadedLevel);
+
+        }
+    }
+}
