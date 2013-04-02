7b3b82a01bafb9974cdda6378b010c0f741d428c
Michael Bo Magling <themikker@hotmail.com>
2013-04-03 00:51:07 +0200

    Map using new dimensions.

--- Assets/Scripts/Map/MapMovementController.cs
+++ Assets/Scripts/Map/MapMovementController.cs
@@ -41,19 +41,19 @@ public class MapMovementController : MonoBehaviour {
         float ym = Input.GetAxis("Mouse Y");
         float multiplier;
         if (zoomedIn)
-            multiplier = 20.0f;
+            multiplier = 40.0f;
         else
-            multiplier = 10.0f;
+            multiplier = 20.0f;
         Vector3 pos = Camera.main.transform.position;
         if (Input.GetMouseButton(0) && !onguidown)
         {
-            if (xm < -0.2f)
+            if (xm < -0.02f)
                 pos.x += xm / multiplier;
-            else if (xm > 0.2f)
+            else if (xm > 0.02f)
                 pos.x += xm / multiplier;
-            if (ym < -0.2f)
+            if (ym < -0.02f)
                 pos.z += ym / multiplier;
-            else if (ym > 0.2f)
+            else if (ym > 0.02f)
                 pos.z += ym / multiplier;
         }
 
@@ -71,33 +71,37 @@ public class MapMovementController : MonoBehaviour {
                 mdown = false;
         }
         if (zoomedIn) {
+            float upper = -2f;
+            float lower = 2f;
             pos.y = 1.361f;
-            if (pos.x < -1.71f)
-                pos.x = -1.71f;
-            else if (pos.x > 1.73f)
-                pos.x = 1.73f;
+            if (pos.x < upper)
+                pos.x = upper;
+            else if (pos.x > lower)
+                pos.x = lower;
             if (pos.z < -1.79f)
                 pos.z = -1.79f;
-            else if (pos.z > 1.78f)
-                pos.z = 1.78f;
+            else if (pos.z > 1.9f)
+                pos.z = 1.9f;
         }
         else {
+            float upper = -0.58f;
+            float lower = 0.4f;
             pos.y = 5f;
-            if (pos.x < -1.58f)
-                pos.x = -1.58f;
-            else if (pos.x > 0f)
-                pos.x = 0f;
+            if (pos.x < upper)
+                pos.x = upper;
+            else if (pos.x > lower)
+                pos.x = lower;
             if (pos.z < -1.05f)
                 pos.z = -1.05f;
-            else if (pos.z > 1.48f)
-                pos.z = 1.48f;
+            else if (pos.z > 1.28f)
+                pos.z = 1.28f;
         }
 
 
         float posx = Camera.main.transform.position.x;
         float posz = Camera.main.transform.position.y;
         float posy = Camera.main.transform.position.z;
-        //print(posx + " " + posy + " " + posz);
+        print(posx + " " + posy + " " + posz);
         //print(Input.mousePosition.x + " " + Input.mousePosition.y);
         Camera.main.transform.position = pos;
     }
