c1fea7d656bfcb67c3c3ea02d93653df82a2a9de
Michael Bo Magling <themikker@hotmail.com>
2013-04-09 14:14:21 +0200

    Temporary in-game GUI

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class GUIScript : MonoBehaviour {
 
-    
+    public GUISkin gSkin;
+    public Texture2D runningSoldiers, runningGoal, damagebar, swordLeft, swordRight, bloodsplatter;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,15 @@ public class GUIScript : MonoBehaviour {
         GUI.Box(new Rect(Screen.width-225, 25, 200, 75), "");
         GUI.Label(new Rect(Screen.width - 225 + 15, 25 + 15, 200, 75), "Score: 9756");
         GUI.Label(new Rect(Screen.width - 225 + 15, 25 + 15*2, 200, 75), "Multiplier: x2");
+
+        GUI.Box(new Rect(15, Screen.height - 65, Screen.width - 30, 50), "");
+        GUI.DrawTexture(new Rect(15, Screen.height - 64, 50, 50), runningSoldiers);
+        GUI.DrawTexture(new Rect(Screen.width-65, Screen.height - 64, 50, 50), runningSoldiers);
+
+        GUI.DrawTexture(new Rect(Screen.width / 2 - 250, Screen.height - 90, 500, 12), damagebar);
+        GUI.DrawTexture(new Rect(Screen.width / 2 - 250 - swordLeft.width / 2, Screen.height - 90 - swordLeft.height / 2 + 6, swordLeft.width, swordLeft.height), swordLeft);
+        GUI.DrawTexture(new Rect(Screen.width / 2 + 250 - swordRight.width / 2, Screen.height - 90 - swordRight.height / 2 + 6, swordRight.width, swordRight.height), swordRight);
+        GUI.DrawTexture(new Rect(Screen.width / 2 - bloodsplatter.width / 2, Screen.height - 90 - bloodsplatter.height / 2 + 6, bloodsplatter.width, bloodsplatter.height), bloodsplatter);
         GUI.EndGroup();
     }
 
@@ -40,6 +50,7 @@ public class GUIScript : MonoBehaviour {
         }
 
         GUI.Label(new Rect(10, 50, 80, 30), "" + Time.timeSinceLevelLoad);
+        GUI.skin = gSkin;
         Level_Interface();
     }
 }
